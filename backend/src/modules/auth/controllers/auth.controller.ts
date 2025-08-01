2d74b413e8afddd52b50adcde7b71d76e98bc75d
= <fayamir54@gmail.com>
2025-08-01 11:26:25 -0700

    Add verify-otp end-point

--- backend/src/modules/auth/controllers/auth.controller.ts
+++ backend/src/modules/auth/controllers/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { ApiOkResponse } from '@nestjs/swagger';
 import { GetOtpDto } from '../dto/get-otp.dto';
 import { getOtpResponse } from '../dto/response/get-otp.response';
+import { LoginResponse } from '../dto/response/login.response';
+import { VerifyOtpDto } from '../dto/verify-otp.dto';
 import { AuthService } from '../services/auth.service';
 
 @Controller('auth')
@@ -12,5 +14,11 @@ export class AuthController {
   @ApiOkResponse({ type: getOtpResponse })
   async getOtp(@Query() dto: GetOtpDto) {
     return await this.authService.getOtp(dto);
+  }
+
+  @Post('verify-token')
+  @ApiOkResponse({ type: LoginResponse })
+  async verifyOtp(@Body() dto: VerifyOtpDto) {
+    return await this.authService.verifyOtp(dto);
   }
 }
