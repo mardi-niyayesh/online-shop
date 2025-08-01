d3ed20d6bfabb90a2bf470e2228d759082f7165c
= <fayamir54@gmail.com>
2025-08-01 11:26:52 -0700

    Add verify-otp services

---
+++ backend/src/modules/auth/dto/verify-otp.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsPhoneNumber, IsString, Length } from 'class-validator';
+
+export class VerifyOtpDto {
+  @ApiProperty({ example: '94356' })
+  @IsString()
+  @Length(5, 5)
+  code: string;
+
+  @ApiProperty({ example: '09921810208' })
+  @IsPhoneNumber('IR')
+  phone: string;
+}
--- backend/src/modules/auth/services/auth.service.ts
+++ backend/src/modules/auth/services/auth.service.ts
@@ -1,10 +1,13 @@
 import { User } from '@app/user/entities/user.entity';
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { GetOtpDto } from '../dto/get-otp.dto';
 import { getOtpResponse } from '../dto/response/get-otp.response';
+import { LoginResponse } from '../dto/response/login.response';
+import { VerifyOtpDto } from '../dto/verify-otp.dto';
 import { Otp } from '../entities/otp.entity';
+import { JwtAppService } from './jwt.service';
 import { OtpService } from './otp.service';
 
 @Injectable()
@@ -15,6 +18,7 @@ export class AuthService {
     @InjectRepository(Otp)
     private readonly otpRepository: Repository<Otp>,
     private readonly otpService: OtpService,
+    private readonly JwtService: JwtAppService,
   ) {}
 
   async getOtp(dto: GetOtpDto): Promise<getOtpResponse> {
@@ -30,6 +34,36 @@ export class AuthService {
 
     return {
       code: newOtpCode,
+    };
+  }
+
+  async verifyOtp(dto: VerifyOtpDto): Promise<LoginResponse> {
+    const otp = (await this.otpService.validateOtp(dto.phone, dto.code)) as Otp;
+
+    const expireResult = this.otpService.CheckExpireOtp(otp);
+
+    if (expireResult) throw new BadRequestException('otp has expired !');
+
+    await this.otpService.removeOtp(otp);
+
+    const user = await this.userRepository.findOne({
+      where: { phone: dto.phone },
+    });
+
+    if (user)
+      return {
+        accessToken: await this.JwtService.generateAccessToken({
+          sub: user.id,
+        }),
+      };
+
+    const newUser = this.userRepository.create({ phone: dto.phone });
+    await this.userRepository.save(newUser);
+
+    return {
+      accessToken: await this.JwtService.generateAccessToken({
+        sub: newUser.id,
+      }),
     };
   }
 }
