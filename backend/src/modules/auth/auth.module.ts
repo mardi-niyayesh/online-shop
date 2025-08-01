c0422891aa8272e529c76846913e55dc97434759
= <fayamir54@gmail.com>
2025-08-01 11:25:59 -0700

    Add new dependencies

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,15 +1,17 @@
 import { User } from '@app/user/entities/user.entity';
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './controllers/auth.controller';
 import { Otp } from './entities/otp.entity';
 import { Role } from './entities/role.entity';
 import { AuthService } from './services/auth.service';
+import { JwtAppService } from './services/jwt.service';
 import { OtpService } from './services/otp.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Role, User, Otp])],
   controllers: [AuthController],
-  providers: [AuthService, OtpService],
+  providers: [AuthService, OtpService, JwtAppService, JwtService],
 })
 export class AuthModule {}
