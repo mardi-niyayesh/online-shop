2696f0e7341b86dfbedbd3b8c7ab503076511c4c
= <fayamir54@gmail.com>
2025-08-01 08:11:39 -0700

    Add first-verison of user-entity

---
+++ backend/src/modules/user/entities/user.entity.ts
@@ -0,0 +1,18 @@
+import { BaseAppEntity } from '@common/entity/base.entity';
+import { Column, Entity } from 'typeorm';
+
+@Entity()
+export class User extends BaseAppEntity {
+  @Column({ nullable: true })
+  firstname: string;
+
+  @Column({ nullable: true })
+  lastname: string;
+
+  @Column({ unique: true })
+  phone: string;
+
+  //Feature
+  @Column()
+  roleId: string;
+}
