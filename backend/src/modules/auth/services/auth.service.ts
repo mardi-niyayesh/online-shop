a48e1ade88ea8c3b9753c54e603390a76a943e62
= <fayamir54@gmail.com>
2025-08-03 13:46:40 -0700

    fix bugs of end-points

--- backend/src/modules/auth/services/auth.service.ts
+++ backend/src/modules/auth/services/auth.service.ts
@@ -24,50 +24,60 @@ export class AuthService {
   async getOtp(dto: GetOtpDto): Promise<getOtpResponse> {
     await this.otpService.checkRate(dto.phone);
 
-    const newOtpCode = this.otpService.generateOtp();
-
-    const newOtp = this.otpRepository.create({
-      phone: dto.phone,
-      code: newOtpCode,
+    const user = await this.userRepository.findOne({
+      where: { phone: dto.phone },
     });
-    await this.otpRepository.save(newOtp);
 
-    return {
-      code: newOtpCode,
-    };
+    if (user) {
+      const newOtpCode = this.otpService.generateOtp();
+      const newOtp = this.otpRepository.create({
+        phone: dto.phone,
+        code: newOtpCode,
+      });
+      await this.otpRepository.save(newOtp);
+      return {
+        code: newOtpCode,
+      };
+    } else {
+      const newUser = this.userRepository.create({
+        firstname: dto.firstname,
+        lastname: dto.lastname,
+        phone: dto.phone,
+      });
+
+      await this.userRepository.save(newUser);
+
+      const newOtpCode = this.otpService.generateOtp();
+      const newOtp = this.otpRepository.create({
+        phone: dto.phone,
+        code: newOtpCode,
+      });
+      await this.otpRepository.save(newOtp);
+      return {
+        code: newOtpCode,
+      };
+    }
   }
 
   async verifyOtp(dto: VerifyOtpDto): Promise<LoginResponse> {
     const otp = (await this.otpService.validateOtp(dto.phone, dto.code)) as Otp;
-
     const expireResult = this.otpService.CheckExpireOtp(otp);
-
     if (expireResult) throw new BadRequestException('otp has expired !');
-
     await this.otpService.removeOtp(otp);
 
     const user = await this.userRepository.findOne({
       where: { phone: dto.phone },
     });
 
-    if (user)
-      return {
-        accessToken: await this.JwtService.generateAccessToken({
-          sub: user.id,
-        }),
-      };
+    if (!user) throw new BadRequestException('You did not recive otp !');
 
-    const newUser = this.userRepository.create({
-      phone: dto.phone,
-      firstname: dto.firstname,
-      lastname: dto.lastname,
-    });
-    await this.userRepository.save(newUser);
+    if (!user.isVerified) {
+      user.isVerified = true;
+      await this.userRepository.save(user);
+    }
 
     return {
-      accessToken: await this.JwtService.generateAccessToken({
-        sub: newUser.id,
-      }),
+      accessToken: await this.JwtService.generateAccessToken({ sub: user.id }),
     };
   }
 }
--- backend/src/modules/auth/services/otp.service.ts
+++ backend/src/modules/auth/services/otp.service.ts
@@ -43,7 +43,7 @@ export class OtpService {
     if (otp) {
       const expireResult = this.CheckExpireOtp(otp);
 
-      if (expireResult)
+      if (!expireResult)
         throw new HttpException(
           'You can get otp every 2 min',
           HttpStatus.TOO_MANY_REQUESTS,
