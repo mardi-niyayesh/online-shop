a4bacc43671c749a5c00c6d2d4acb25c9144eaf4
niyayesh <niyayes@gmail.com>
2025-08-05 14:12:15 +0330

    logic send-in js

---
+++ frontend/Login/send-code.js
@@ -0,0 +1,42 @@
+
+const $=document
+
+const sendCodeBtn=$.querySelector(".form-sign")
+const inpGetCode=$.querySelector(".username")
+const getLocalPhone=localStorage.getItem("phone")
+
+sendCodeBtn.addEventListener("click",(e)=>{
+    e.preventDefault()
+     fetch("http://localhost:3000/api/auth/verify-token",{
+      method:"POST",
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(
+        {
+            code:inpGetCode.value,
+            phone:getLocalPhone
+
+        }
+    )
+    
+     })
+    .then(res=>res.json())
+    .then((data)=>{
+        if(data.accessToken){
+            localStorage.setItem("Token",data.accessToken)
+            console.log("توکن یافت شد");
+        }else{
+           console.log("توکن یافت نشد");
+        }
+        console.log(data);
+        
+        
+    })
+    .catch(err=>console.error("کد وارد شده صحیح نمیباشد:"+err))
+
+    localStorage.removeItem("phone")
+    inpGetCode.value=""
+})
+
+
