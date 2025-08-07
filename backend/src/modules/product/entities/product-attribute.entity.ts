d37884a2ca407ec4d9f2e63fe2f0d8d657e4bfc6
= <fayamir54@gmail.com>
2025-08-07 05:39:01 -0700

    add product-attribute-entity

---
+++ backend/src/modules/product/entities/product-attribute.entity.ts
@@ -0,0 +1,30 @@
+import { BaseAppEntity } from '@common/entity/base.entity';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
+import { Product } from './product.entity';
+
+@Entity()
+export class ProductAttribute extends BaseAppEntity {
+  @Column({ nullable: true })
+  size: string;
+
+  @Column({ nullable: true })
+  color: string;
+
+  @Column({ nullable: true })
+  heigth: number;
+
+  @Column({ nullable: true })
+  mentality: string;
+
+  @Column({ nullable: true })
+  design: string;
+
+  @ManyToOne(() => Product, (product) => product.attributes, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'productId' })
+  product: Product;
+
+  @Column()
+  productId: number;
+}
