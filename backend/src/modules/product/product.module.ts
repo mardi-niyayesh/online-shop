274fa8a48b24e0bbea225a8ac92324bea74a5f23
= <fayamir54@gmail.com>
2025-08-09 14:55:08 -0700

    add new dependencies

--- backend/src/modules/product/product.module.ts
+++ backend/src/modules/product/product.module.ts
@@ -1,12 +1,15 @@
+import { S3Service } from '@common/services/s3.service';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductCategoryController } from './controller/product-category.controller';
+import { ProductController } from './controller/product.controller';
 import { Discount } from './entities/discount.entity';
 import { ProductAttribute } from './entities/product-attribute.entity';
 import { ProductCategory } from './entities/product-category.entity';
 import { Product } from './entities/product.entity';
 import { Rate } from './entities/rate.entity';
 import { ProductCategoryService } from './service/product-category.service';
+import { ProductService } from './service/product.service';
 
 @Module({
   imports: [
@@ -18,7 +21,7 @@ import { ProductCategoryService } from './service/product-category.service';
       Rate,
     ]),
   ],
-  controllers: [ProductCategoryController],
-  providers: [ProductCategoryService],
+  controllers: [ProductCategoryController, ProductController],
+  providers: [ProductCategoryService, ProductService, S3Service],
 })
 export class ProductModule {}
