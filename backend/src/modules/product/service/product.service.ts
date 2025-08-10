1d3081bf653af18984b3ac1f14052f8a7682d065
= <fayamir54@gmail.com>
2025-08-10 01:56:11 -0700

    add findAll service

--- backend/src/modules/product/service/product.service.ts
+++ backend/src/modules/product/service/product.service.ts
@@ -1,6 +1,12 @@
 import { S3Service } from '@common/services/s3.service';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import {
+  FilterOperator,
+  paginate,
+  Paginated,
+  PaginateQuery,
+} from 'nestjs-paginate';
 import { DeepPartial, Repository } from 'typeorm';
 import { CreateProductDto } from '../dto/product/create-product.dto';
 import { Product } from '../entities/product.entity';
@@ -32,5 +38,42 @@ export class ProductService {
 
       return await this.productRepository.save(newProduct);
     }
+  }
+
+  async findAll(query: PaginateQuery): Promise<Paginated<Product>> {
+    return paginate(query, this.productRepository, {
+      sortableColumns: ['createdAt'],
+      defaultSortBy: [['createdAt', 'DESC']],
+      relations: { attributes: true, rates: true, category: true },
+      select: [
+        'id',
+        'createdAt',
+        'updatedAt',
+        'name',
+        'description',
+        'price',
+        'image',
+        'categoryId',
+        'rates.rate',
+        'rates.userId',
+        'category.title',
+        'category.description',
+        'attributes.size',
+        'attributes.color',
+        'attributes.height',
+        'attributes.mentality',
+        'attributes.design',
+        'attributes.stock',
+      ],
+      filterableColumns: {
+        name: [FilterOperator.EQ],
+        categoryId: [FilterOperator.EQ],
+        'attributes.size': [FilterOperator.EQ],
+        'attributes.color': [FilterOperator.EQ],
+        'attributes.height': [FilterOperator.EQ],
+        'attributes.mentality': [FilterOperator.EQ],
+        'attributes.design': [FilterOperator.EQ],
+      },
+    });
   }
 }
