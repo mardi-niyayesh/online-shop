d5ab9d891df5a2da909098159ced57be2943eebc
niyayesh <niyayes@gmail.com>
2025-08-11 15:20:33 +0330

    update menue hambergr in header

--- frontend/component-product/header/header.js
+++ frontend/component-product/header/header.js
@@ -1,3 +1,14 @@
 
 
+const sidbar=document.querySelector(".side-bar ul")
+const showmenue=document.querySelector(".showmenue")
 
+function hidSidbar(){
+          sidbar.style.display="none"
+}
+
+ showmenue.addEventListener("click",()=>{
+    sidbar.style.display="flex"
+    console.log("niyayesj");
+    
+ })
