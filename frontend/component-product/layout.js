acf7ef62ba3caa3f850bb2a64ec22771fb1e16cb
niyayesh <niyayes@gmail.com>
2025-08-11 15:26:48 +0330

    update layout js

--- frontend/component-product/layout.js
+++ frontend/component-product/layout.js
@@ -19,7 +19,7 @@ function loadHTML(elementId, url) {
 
 loadHTML('header', './component-product/header/header.html');
 loadHTML('main', './component-product/main/off/off.html');
-loadHTML('main', './component-product/main/product/product.html');
+loadHTML('main', './component/product/product.html');
 loadHTML('main', './component-product/main/inestagram/insta.html');
 loadHTML('main', './component-product/main/comment/comment.html');
 loadHTML('footer', './component-product/footer/footer.html');
