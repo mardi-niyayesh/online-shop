0963fa484ae66daa2f06f2a39b035e166326eb1f
= <fayamir54@gmail.com>
2025-08-20 06:50:29 -0700

    update all dto for feedbacks

--- backend/src/modules/product/dto/feedback/update-feedback.dto.ts
+++ backend/src/modules/product/dto/feedback/update-feedback.dto.ts
@@ -1,14 +1,20 @@
 import { Product } from '@app/product/entities/product.entity';
 import { Exists } from '@common/validators/is-exist.validator';
 import { ApiProperty } from '@nestjs/swagger';
-import { IsInt, IsOptional, Max, Min } from 'class-validator';
+import { IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
 
 export class UpdateFeedbackDto {
   @ApiProperty()
   @IsInt()
   @Min(1)
   @Max(5)
+  @IsOptional()
   rate: number;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  message: string;
 
   @ApiProperty()
   @Exists(Product)
