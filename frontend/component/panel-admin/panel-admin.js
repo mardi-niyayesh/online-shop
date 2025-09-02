6a82c7b79f8e60c80cdb31d21223d50ddf124c18
niyayesh <niyayeshammardii@gmail.com>
2025-09-02 13:38:54 +0330

    creat fetch to panel add product

--- frontend/component/panel-admin/panel-admin.js
+++ frontend/component/panel-admin/panel-admin.js
@@ -1,3 +1,4 @@
+import{fetchProductsByCategory}from "/frontend/component/product/product.js"
 const $=document
 const getToken=localStorage.getItem("Token")
 const containerli=$.querySelectorAll(".container-li")
@@ -9,6 +10,10 @@ const priceProduct=$.querySelector("#price-product")
 const description=$.querySelector("#description")
 const category=$.querySelector("#category")
 const imageProduct=$.querySelector("#image-product")
+const addCategory=$.querySelector('.add-category')
+const addCat=$.querySelector('.add-cat-panel')
+const btnRegisterCategory=$.querySelector('.btn-re-cat')
+const namecategory=$.querySelector('.name-category')
 
 //Chang background color on mouseover
 containerli.forEach(element => {
@@ -21,10 +26,10 @@ containerli.forEach(element => {
   });
 });
 
+
+// click submit btn for add product to panel admin
 addProductForm.addEventListener("submit",(event)=>{
 event.preventDefault()
-
- const formData = new FormData();
 
  if(!nameProduct.value || !priceProduct.value || !category.value){
     alert("لطفا همه فیلدهای ضروری را پر کنید");
@@ -35,13 +40,17 @@ if(Number(priceProduct.value) <= 0){
     alert("قیمت باید عددی صحیح و بزرگتر از صفر باشد");
     return;
 }
- formData.append('name',nameProduct.value)
- formData.append('price',Number(priceProduct.value))
- formData.append('description',description.value)
- formData.append('categoryId ',category.value)
- formData.append('image',imageProduct.files[0])
+ const formData = new FormData();
+formData.append('name', nameProduct.value.toString());
+formData.append('price', priceProduct.value.toString()); 
+formData.append('description', description.value);
+formData.append('categoryId', category.value.toString()); 
+if (imageProduct.files.length > 0) {
+  formData.append('image', imageProduct.files[0]);
+}
 
 fetchaddProduct(formData)
+// fetchProductsByCategory()
 })
 
 //add product from panel to products
@@ -50,20 +59,25 @@ async function fetchaddProduct(formData){
 try{
    const response=await fetch(`http://localhost:3000/api/products`,{
     method: 'POST',
-    body: formData,
     headers:{
        'Authorization': `Bearer ${getToken}`
-    }
+    },
+      body: formData,
+      // body:JSON.stringify({
+      //   name:nameProduct.value,
+      //   price:priceProduct.value,
+      //   description:description.value,
+      //   categoryId:category.value
+      // })
   })
 
  const data = await response.json();
-
   if(!data.ok){
-    console.log("نمی توانیم محصول رو اضافه کنیم متاسفیم:(");
+    console.log("نمی توانیم محصول رو اضافه کنیم متاسفیم:(",data.message);
     
   }
-  console.log(data);
-  return data
+  console.log('محصول با موفقیت اضافه شد:',data);
+  
   }
   catch(err){
     console.log("خطا در ساختن محصولات:",err);
@@ -71,4 +85,79 @@ try{
   }
 }
 
+//get ALL categores by server
+async function fetchGetCategory(){
+  try{
+ const response=await fetch(`http://localhost:3000/api/product-category`,{
+  method: 'GET',
+  headers: {
+        'Authorization': `Bearer ${getToken}`,
+        'Content-Type': 'application/json'
+      }  
+  })
 
+  const data=await response.json()
+  if(Array.isArray(data.data)){
+    category.innerHTML ='<option value="">select category</option>'
+    data.data.forEach(cat=>{
+    const optionElem=document.createElement('option')
+     optionElem.textContent = cat.title;
+     optionElem.value=cat.id;
+     category.append(optionElem)
+    
+    })
+  }
+  }
+  catch(err){
+   console.log('متاسفیم کتگوری ها لود نشد :(',err);
+   
+  }
+}
+
+//show form new category by click add category
+addCategory.addEventListener("click",(event)=>{
+  event.preventDefault()
+  addCat.style.display ='block'
+  namecategory.focus();
+})
+
+//send request for creat new category by click btnregister
+btnRegisterCategory.addEventListener('click',(event)=>{
+  event.preventDefault()
+  if( namecategory.value === ""){
+    alert("لطفا یک کتگوری انتخاب کنید این فیلد نمی تواند خالی باشد")
+    return false
+  }
+   addCat.style.display ='none'
+  fetchCreatCategory()
+})
+
+//creat category by panelAdmin
+ async function fetchCreatCategory(){
+    try{
+      const response= await fetch(`http://localhost:3000/api/product-category`,{
+    method:'POST',
+     headers: {
+        'Authorization': `Bearer ${getToken}`,
+         'Content-Type': 'application/json' 
+      },
+      body:JSON.stringify({
+        title:namecategory.value
+      })
+    })
+    const data=await response.json()
+    console.log("شما با موفقیت کتگوری جدید رو ایجاد کردید",data);
+    namecategory.value = ''
+    fetchGetCategory()
+   
+    }
+    catch(err){
+    console.log('متاسفیم کتگوری شما ساخته نشد',err);
+    }
+}
+
+
+//call functions
+window.addEventListener('DOMContentLoaded',()=>{
+  fetchGetCategory()
+})
