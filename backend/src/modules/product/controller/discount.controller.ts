d2179a094e11c4b2b014c01caf3518e8f0b97241
= <fayamir54@gmail.com>
2025-09-03 08:47:45 -0700

    add findAll-discounts end-point

--- backend/src/modules/product/controller/discount.controller.ts
+++ backend/src/modules/product/controller/discount.controller.ts
@@ -1,5 +1,7 @@
+import { PaginationOptions } from '@common/decorators/pagination-options.decorator';
 import { AtLeastOneFieldPipe } from '@common/pipe/at-least-one.pipe';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Paginate, PaginateQuery } from 'nestjs-paginate';
 import { CreateDiscountDto } from '../dto/discount/create-discount.dto';
 import { ExpVsGlobalDiscountPipe } from '../pipes/exp-discount.pipe';
 import { DiscountService } from '../service/discount.service';
@@ -17,5 +19,18 @@ export class DiscountController {
     dto: CreateDiscountDto,
   ) {
     return await this.discountService.create(dto);
+  }
+
+  @Get()
+  @PaginationOptions({
+    filterOptions: [
+      { field: 'productId', example: '$eq:2' },
+      { field: 'categoryId', example: '$eq:1' },
+    ],
+
+    sortOptions: [{ example: 'createdAt:DESC' }],
+  })
+  async findAll(@Paginate() query: PaginateQuery) {
+    return await this.discountService.findAll(query);
   }
 }
--- backend/src/modules/product/service/discount.service.ts
+++ backend/src/modules/product/service/discount.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import {
+  FilterOperator,
+  paginate,
+  Paginated,
+  PaginateQuery,
+} from 'nestjs-paginate';
 import { DeepPartial, Repository } from 'typeorm';
 import { CreateDiscountDto } from '../dto/discount/create-discount.dto';
 import { Discount } from '../entities/discount.entity';
@@ -15,5 +21,17 @@ export class DiscountService {
     const newDiscount = this.discountRepository.create(dto);
 
     return await this.discountRepository.save(newDiscount);
+  }
+
+  async findAll(query: PaginateQuery): Promise<Paginated<Discount>> {
+    return await paginate(query, this.discountRepository, {
+      sortableColumns: ['createdAt'],
+      defaultSortBy: [['createdAt', 'DESC']],
+      relations: { product: true, category: true },
+      filterableColumns: {
+        productId: [FilterOperator.EQ],
+        categoryId: [FilterOperator.EQ],
+      },
+    });
   }
 }
