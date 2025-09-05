e8e905a5fda7ce909f298f9dc4640d5ff8052bdd
= <fayamir54@gmail.com>
2025-09-05 07:39:18 -0700

    add new dependencies

--- backend/src/modules/basket/basket.module.ts
+++ backend/src/modules/basket/basket.module.ts
@@ -1,6 +1,8 @@
+import { JwtAppService } from '@app/auth/services/jwt.service';
 import { Product } from '@app/product/entities/product.entity';
 import { User } from '@app/user/entities/user.entity';
 import { Module } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BasketController } from './controllers/basket.controller';
 import { BasketItem } from './entities/basket-item.entity';
@@ -10,6 +12,6 @@ import { BasketService } from './services/basket.service';
 @Module({
   imports: [TypeOrmModule.forFeature([Basket, BasketItem, User, Product])],
   controllers: [BasketController],
-  providers: [BasketService],
+  providers: [BasketService, JwtAppService, JwtService],
 })
 export class BasketModule {}
